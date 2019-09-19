4b50ebbe06457c874a1fb4aebdf9cdb42a96c81d
sowens-csd <stephen.owens@thoughtwire.com>
2019-09-19 09:09:11 -0400

    rf(lip): Upgrade to Flutter 1.9

--- android/src/main/kotlin/com/example/local_image_provider/LocalImageProviderPlugin.kt
+++ android/src/main/kotlin/com/example/local_image_provider/LocalImageProviderPlugin.kt
@@ -1,19 +1,9 @@
 package com.example.local_image_provider
 
 import android.app.Activity
-import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.ImageDecoder
 import android.net.Uri
-import android.os.Build
 import android.provider.MediaStore
-import com.bumptech.glide.Glide
-import com.bumptech.glide.GlideBuilder
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.module.AppGlideModule
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.signature.ObjectKey
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -22,9 +12,7 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 import org.json.JSONObject
 import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
 import java.util.*
-import java.util.concurrent.Future
 import kotlin.collections.ArrayList
 
 class LocalImageProviderPlugin ( activity: Activity): MethodCallHandler {
---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/stephen.owens/Documents/dev/flutter/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/stephen.owens/Documents/dev/csd/folktells/vsprojects/local_image_provider/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/stephen.owens/Documents/dev/flutter/sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=1.0.1"
